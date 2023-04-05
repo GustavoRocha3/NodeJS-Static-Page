39d8ff7a9b441ebba8b16ff7726a1c3f19a6f81e
Gustavo de Alencar Barros Rocha <gustavo.rocha@sinergytech.local>
2023-04-04 22:42:28 -0300

    deixando o site dinâmico

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser');
+
 const path = require('path')
 
 const app = express();
 
+const Posts = require('./Posts')
+
+mongoose.connect('mongodb+srv://root:tKl8SZeV7pEFIyQS@cluster0.nkyzl.mongodb.net/dankicode?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
+    console.log('Conectado com sucesso');
+}).catch((err)=>{
+    console.log(err.message);
+});
+
 app.use( bodyParser.json() );
 app.use(bodyParser.urlencoded({
     extended: true
-}))
+}));
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
@@ -19,7 +29,22 @@ app.get('/', (req,res) => {
     // console.log(req.query.busca)
 
     if(req.query.busca == null){
-        res.render('home',{});
+        Posts.find({}).exec()
+        .then((posts) => {
+            posts = posts.map((val) =>{
+                return {
+                    titulo: val.titulo,
+                    conteudo: val.conteudo,
+                    descricaoCurta: val.conteudo.substr(0,100),
+                    imagem: val.imagem,
+                    slug: val.slug,
+                    categoria: val.categoria
+                }
+            })
+            res.render('home',{posts:posts});
+        }).catch((err) => {
+            console.log(err.message);
+        })
     }else{
         res.render('busca',{});
     }
@@ -28,7 +53,17 @@ app.get('/', (req,res) => {
 
 app.get('/:slug', (req,res) => {
     // res.send(req.params.slug);
-    res.render('single',{});
+    Posts.findOneAndUpdate({slug: req.params.slug}, {$inc: {views: 1}}, {new: true})
+    .then(resposta => {
+        console.log(resposta);
+        res.render('single',{noticia:resposta});
+    })
+    .catch(err => {
+        console.log(err.message)
+    })
+        
+    
+
 })
 
 app.listen(5000, () =>{
